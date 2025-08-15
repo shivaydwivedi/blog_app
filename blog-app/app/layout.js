9a3a924a9a92e9a6cba4a732830bc9cb3e945e27
shivaydwivedi <polymath1510@gmail.com>
2025-08-16 01:24:11 +0530

    Add sharp for production image optimization

--- blog-app/app/layout.js
+++ blog-app/app/layout.js
@@ -1,7 +1,7 @@
 import { Outfit } from 'next/font/google'
 import './globals.css'
 
-const outfit = Outfit({ subsets: ['latin'] ,weight:["400","500","600","700"] })
+const outfit = Outfit({ subsets: ['latin'], weight: ["400", "500", "600", "700"] })
 
 export const metadata = {
   title: 'Blog App',
